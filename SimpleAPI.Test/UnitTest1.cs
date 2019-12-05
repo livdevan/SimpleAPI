400209599b394e4aedb7266febc2eb40e3bcbbc3
Livan HAGI OSMAN <livdevan@gmail.com>
2019-12-05 20:11:41 +0100

    Ajoutez des fichiers projet.

---
+++ SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(5);
+            Assert.Equal("Michael Jackson", returnValue);
+        }
+    }
+}
